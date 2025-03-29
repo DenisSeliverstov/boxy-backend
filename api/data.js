b7e1825d352d56a1e417dc94cfbc0d102523cbc8
Denis Seliverstov <sfaiross@yandex.ru>
2025-03-30 01:43:02 +0300

    Fixed bugs

--- api/data.js
+++ api/data.js
@@ -1,5 +1,13 @@
-
 export default function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // Разрешаем доступ с любого источника
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); // Разрешаем методы
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Разрешаем определенные заголовки
+  
+    if (req.method === "OPTIONS") {
+      res.status(200).end(); // Отвечаем на OPTIONS запросы
+      return;
+    }
+  
     if (req.method === "GET") {
       res.status(200).json({ message: "Привет от бекенда!" });
     } else {
